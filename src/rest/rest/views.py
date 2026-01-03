5e9ef6723a5026feaabd18bf4b550297027a0840
nitesh0512 <niteshsoni0512@gmail.com>
2026-01-03 16:15:22 +0530

    Add todo apis

--- src/rest/rest/views.py
+++ src/rest/rest/views.py
@@ -7,14 +7,22 @@ from pymongo import MongoClient
 
 mongo_uri = 'mongodb://' + os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]
 db = MongoClient(mongo_uri)['test_db']
+todos = db['todos']
 
+  
 class TodoListView(APIView):
 
     def get(self, request):
         # Implement this method - return all todo items from db instance above.
-        return Response({}, status=status.HTTP_200_OK)
+        res = []
+        for todo in todos.find():
+            dict_todo = {"todo": todo["todo"]}
+            res.append(dict_todo)
+        return Response(res, status=status.HTTP_200_OK, content_type='application/json')
         
     def post(self, request):
         # Implement this method - accept a todo item in a mongo collection, persist it using db instance above.
-        return Response({}, status=status.HTTP_200_OK)
+        todo = { "todo": request.data.get("todo") }
+        todos.insert_one(todo)
+        return Response(True, status=status.HTTP_200_OK, content_type='application/json')
 
